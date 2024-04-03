5877c8e06c0cc3fada111c5046b749b813aae615
Yin <doyin@deloitte.com.cn>
2024-04-03 14:15:12 +0800

    initial version

---
+++ index.js
@@ -0,0 +1,7 @@
+const getArraySum = require('./getArraySum')
+const getRandomInt = require('./getRandomInt')
+const formatDate = require('./formatDate')
+
+module.exports = (arr) => {
+    return Math.max(...arr);
+  }
